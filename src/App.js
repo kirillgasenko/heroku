88f41e5afad614c626b7938b66a1e17c407727d9
Крыжнев Андрей Вячеславович <andrey_kryzhnev@epam.com>
2022-02-01 22:07:21 +0300

    qw

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">nice</div>;
+  return <div className="App">12e12e12e12e</div>;
 }
 
 export default App;
